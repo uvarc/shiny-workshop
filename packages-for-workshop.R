6175b6c99b7400a2dd66347caf08f8d615bd80eb
cagancayco <christina.gancayco@gmail.com>
2020-02-11 09:32:58 -0500

    adding required packages

---
+++ packages-for-workshop.R
@@ -0,0 +1,19 @@
+# packages that  should be installed
+package_list <- c(
+  "devtools",        # you likely have this
+  "tidyverse",       # ditto
+  "rmarkdown",       # ditto
+  "knitr",           # ditto
+  "shiny",           # ditto
+  "shinyWidgets",    # workshop specific
+  "shinyjs",         # workshop specific
+  "flexdashboard",   # workshop specific
+  "miniUI",          # workshop specific
+  "learnr",          # workshop specific
+  "plotly",          # workshop specific
+  "revealjs",        # just for compiling slide collection
+  "mosaic"           # some data sets, etc
+  )
+
+# Run this 
+ install.packages(package_list)
